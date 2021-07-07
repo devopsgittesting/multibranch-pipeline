0298c6e1996184c017d73907ebbebbdf804ac291
devopsgittesting <85344211+devopsgittesting@users.noreply.github.com>
2021-07-07 10:53:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from main branch');
+  res.send('Hello World from main branchb ..');
 });
 
 app.listen(PORT, HOST);
